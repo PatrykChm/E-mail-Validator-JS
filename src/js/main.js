e697998a9babbe5f6d7c203fe63d9de2e64654cc
Patryk <patrykchmielewskipl@gmail.com>
2022-11-04 11:35:59 +0100

    finish JS and add some fun confetti effect

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+const mailInput = document.querySelector('.card__input')
+const sendButton = document.querySelector('.card__button')
+const error = document.querySelector('.card__error')
+const confirm = document.querySelector('.card__confirm')
+
+const clearError = () => {
+	error.classList.remove('error-active')
+	confirm.classList.remove('confirm-active')
+}
+
+const checkEmail = () => {
+	clearError()
+
+	const reg =
+		/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+	if (reg.test(mailInput.value)) {
+		confirm.classList.add('confirm-active')
+
+		startConfetti()
+
+		setTimeout(() => {
+			stopConfetti()
+		}, 2000)
+	} else {
+		error.classList.add('error-active')
+	}
+}
+
+sendButton.addEventListener('click', checkEmail)
